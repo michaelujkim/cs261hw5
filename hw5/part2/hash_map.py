4d776f467e2e70d838eb8c61cf48654846c2ff55
michaelujkim <michaelujkim@gmail.com>
2020-06-01 16:20:19 -0700

    100/100 comments

--- hw5/part2/hash_map.py
+++ hw5/part2/hash_map.py
@@ -2,6 +2,9 @@
 # ===================================================
 # Implement a hash map with chaining
 # ===================================================
+#Name: Michael Kim
+#class: CS261-400
+#Date: Jun 1 2020
 
 class SLNode:
     def __init__(self, key, value):
@@ -117,13 +120,19 @@ class HashMap:
         for i in range(capacity):
             self._buckets.append(LinkedList())
         self.size=0
+        #basically just creates new list with same capacity
 
     def get_helper(self,key,node):
+        """helper function for the get function. accepts the key and the node in the linked list
+        returns the value if present, or returns false if reaches end of list and doesnt find key"""
         if node==None:
+            #if reaches the end of list, returns false
             return False
         elif node.key==key:
+            #if the current node key and the entered key is the same, returns the nodes value
             return node.value
         else:
+            #recursively iterates through to the next node in hte linked list
             return self.get_helper(key,node.next)
 
     def get(self, key):
@@ -135,11 +144,17 @@ class HashMap:
             The value associated to the key. None if the link isn't found.
         """
         hash= self._hash_function(key)
+        #creates hash
         remain=hash%self.capacity
+        #finds the modulo remainder for the index in the hash table
         thebucket=self._buckets[remain]
+        #names the entered bucket with correct hash modulo output
         return self.get_helper(key,thebucket.head)
+    #returns the get helper to search individual linked lists.
 
     def listmaker(self,node,list):
+        '''recursive helper function to take node and add to end of linked list
+        or if it reaches end of list, does nothing'''
         if node==None:
             return
         else:
@@ -156,45 +171,56 @@ class HashMap:
 
         linklist=[]
 
-
+        #creates a flexible iterable list to fill with values and store
         for x in self._buckets:
+            #iterates through the hash table and adds values to the created list
             if x.head!=None:
                 self.listmaker(x.head,linklist)
+                #calls the above function to fill list
         self._buckets = []
+        #empties the buckets
         self.capacity=capacity
+        #sets capacity to new capacity
         for i in range(capacity):
             self._buckets.append(LinkedList())
+        #fills the buckets with linked lists
         self.size = 0
-
+        #sets size to zero
         for x in linklist:
+            #iterates through the created "store" list and fills the empty hash table again
             value=x.value
+            #setting variables for the put() function
             key=x.key
             hash = self._hash_function(key)
             remain = hash % self.capacity
+            #re calculating each hash
             if self._buckets[remain].head == None:
+                #stolen from the put code
                 self._buckets[remain].add_front(key, value)
                 self.size += 1
 
             else:
-
+                #recursively calls the put helper
                 self.put_helper(key, value, self._buckets[remain].head, remain=remain)
 
 
     def put_helper(self, key, value, node,remain):
+        '''helper for put function. intakes the key and value pair, the current node and the hash modulo output'''
         if node==None:
-
+            #if reaches the end of list and the key is not found, add the key value pair to linked list
             self._buckets[remain].add_front(key,value)
             self.size += 1
-
+            #and increases size
 
 
         elif node.key==key:
             node.value = value
 
-
+        #if the key is found, updates the value to new inputted value
 
 
         else:
+            #recursively calls the put helper with the key value pair and the next value
             return self.put_helper(key,value,node.next,remain=remain)
     def put(self, key, value):
         """
@@ -209,25 +235,33 @@ class HashMap:
         """
 
         hash=self._hash_function(key)
+        #calculates hash
         remain=hash%self.capacity
+        #calculates the hash modulo output
         if self._buckets[remain].head ==None:
+            #if that index in the hash table is empty, add the key value pair
             self._buckets[remain].add_front(key,value)
             self.size+=1
-
+            #increases size
         else:
-
+            #or recursively looks as the index in the hash table with the helper function and
+            #carries out actions there
             return self.put_helper(key,value,self._buckets[remain].head, remain=remain)
 
 
     def remove_helper(self,key,node,remain):
+        '''helper function for remove. intakes the key to be deleted, the current node in the linked list
+        and the hash modulo output'''
         if node==None:
+            #if it reaches the end of the list and doesnt find key, does nothing
             return
         elif node.key==key:
-
+            #if it find the key, removes value using linked list functions
 
             self._buckets[remain].remove(key)
         else:
             return self.remove_helper(key,node.next,remain)
+        #recursively calls the next node
     def remove(self, key):
         """
         Removes and frees the link with the given key from the table. If no such link
@@ -237,10 +271,13 @@ class HashMap:
             key: they key to search for and remove along with its value
         """
         hash = self._hash_function(key)
+        #calculates hash
         remain = hash % self.capacity
+        #calculates the modulo output
         thebucket = self._buckets[remain]
+        #names the bucket in question
         return self.remove_helper(key, thebucket.head,remain=remain)
-
+        #recursively calls the remove hlper to find the key in the linked list in question
     def contains_key(self, key):
         """
         Searches to see if a key exists within the hash table
@@ -250,12 +287,17 @@ class HashMap:
 
         """
         count=0
+        #counter to see if the key is in the table
         for i in self._buckets:
+            #iterates through table and
             if i.contains(key)==None:
+                #if the key is not in the linked list does nothing
                 count+=0
             else:
+                #if it is, increases the count
                 count+=1
         if count >0:
+            #if the count is non-zero, ther key is in the table and returns true, false otherwise
             return True
         else:
             return False
@@ -267,10 +309,13 @@ class HashMap:
 
         """
         counter=0
+        #counter variable for the number of empty buckets
         for i in self._buckets:
+            #iterates through the hash table and increments the counter if an empty bucket is found
             if i.head==None:
                 counter+=1
         return counter
+    #returns counter variable
     def table_load(self):
         """
         Returns:
@@ -278,12 +323,17 @@ class HashMap:
 
         """
         links=0
+        #the counter variable for the number of links in the hash table
         buckets=0
         for i in self._buckets:
+            #iterates through the hashtable and
             if i !=[]:
+                #if the bucket is not empty, add the size of the list to the links counter
                 links+=i.size
             buckets+=1
+            #adds to the bucket counter regardles if empty or not
         return float(links/buckets)
+    #returns the appropriate ratio as a float
 
     def __str__(self):
         """
@@ -297,16 +347,5 @@ class HashMap:
             index = index + 1
         return out
 
-'''m = HashMap(10, hash_function_1)
 
-
-test_values = [("test_5", 5), ("test_-5", -5), ("test_5_", 5), ("diff_word", 15), ("another_word", 20),
-                       ("set", 10), ("anotha_one", -7), ("completely_different", 5), ("getting_there", -1)]
-for key, val in test_values:
-    m.put(key, val)
-
-m.resize_table(5)
-
-
-print(m)'''
 
--- hw5/part2/word_count.py
+++ hw5/part2/word_count.py
@@ -5,6 +5,9 @@
 # counter will return the top X words, as indicated
 # by the user.
 # ===================================================
+#Name: Michael Kim
+#class :CS 261 -400
+#date June 1 2020
 
 import re
 from hash_map import HashMap
@@ -43,7 +46,7 @@ def top_words(source, number):
     keys = []
 
     ht = HashMap(100,hash_function_2)
-    '''def bucket_helper(node)=function()'''
+
 
     # This block of code will read a file one word as a time and
     # put the word in `w`. It should be left as starter code.
@@ -52,49 +55,56 @@ def top_words(source, number):
             words = rgx.findall(line)
             for w in words:
                 keys.append(w.lower())
+                #appends the individual words to the list of words shown above
 
-
+        #preliminary loop
         for i in set(keys):
-
+            #iterates through the words once and puts every unique word in the hash table
+            #if the word is repeated, the value in the hash is merely updated to 0
             ht.put(i,0)
 
-
+        #counting loop will count the words
         for i in keys:
+            #iterates through list and retrieves the associated value
             value=ht.get(i)
             value+=1
+            #increases the value by one
             ht.put(i,value)
-
+            #replaces the old value with the new incremented value
         sortedList=[]
+        #new list of words and word counts
+        #appending iterator
         for i in keys:
+            #gets every value in the hash table
             value=ht.get(i)
-
+            #appends the above list with each key value pair as a list
             sortedList.append([i,value])
 
 
-
+        #to remove duplicated hash values
         res=[]
         for i in sortedList:
+            #for every entry in list
             if i not in res:
+                #if theres a repetition, not included
                 res.append(i)
+        #function to sort the list from greatest to least
         def Sort(list):
             list.sort(key=lambda x: x[1], reverse=True)
             return list
         Sort(res)
-
+        #use it on the filtered list
         res=res[:number]
+        #cut the list down to the appropriate number of entries
         final_list=[]
 
 
-
+        #final list for the tuples
         for i in res:
 
-
+        #iterates and appends list as tuples
              final_list.append( tuple(i))
         return final_list
 
 
 
-'''print(top_words("alice.txt",10))  # COMMENT THIS OUT WHEN SUBMITTING TO GRADESCOPE
-
-print(hash_function_2("character"))
-print(hash_function_2("repeating"))'''
